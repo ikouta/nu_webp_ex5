637e789833029cf85ec0d69003b37b14dd9b2f27
ikouta <72364345+ikouta@users.noreply.github.com>
2021-01-05 11:45:36 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function result() {
 
   if(document.getElementById("uk").checked) {
     let weight = Math.round(300/14) + ' stone';
-    let temperature =  Math.round(94-32/1.8) + ' centigrade';
+    let temperature =  Math.round((94-32)/1.8) + ' centigrade';
     newStory = newStory.replace('300 pounds',weight);
     newStory = newStory.replace('94 fahrenheit',temperature);
   }
